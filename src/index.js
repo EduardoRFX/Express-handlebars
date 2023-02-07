6b5540c6de860ca4f475d3fa6321cd6ef04e641b
EduardoRFX <skyyyta@gmail.com>
2023-02-06 21:31:56 -0300

    'commit'

--- src/index.js
+++ src/index.js
@@ -7,38 +7,48 @@ const app = express()
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
+app.use(express.static("img"))
 
 const produtos = new Array(10).fill('').map((item, index) => ({nome: `Produto ${index}`, preco: Math.random().toString().substring(0,5)}))
 
-app.get('/', (req, res) => {
-    console.table(produtos)
-    let sprodutos = produtos.map((produto, index) => {
+// app.get('/', (req, res) => {
+//     console.table(produtos)
+//     let sprodutos = produtos.map((produto, index) => {
         
-        return {
-            nome: `Produto ${index + 1}`,
-            preco: produto.preco
-        }
-    })
+//         return {
+//             nome: `Produto ${index + 1}`,
+//             preco: produto.preco
+//         }
+//     })
+//     res.render('index', {
+//         layout: false,
+//         showtitle: 'AQUIII',
+//         novo: {
+//             p1 : 1,
+//             p2 : 2
+//         },
+//         lista: [
+//             'Item1',
+//             'Item2',
+//             'Item3',
+//             'Item4',
+//             'Item5',
+//         ],
+//         helpers: {
+//             teste() {
+//                 return 'TESTE conteudo!'
+//             }
+//         },
+//         produtos: sprodutos
+//     })
+// })
+
+app.get('/', (req, res) => {
+    imageList = [];
+    imageList.push({ src: "../img/frs.png", name: "frsLogo" });
     res.render('index', {
         layout: false,
-        showtitle: 'AQUIII',
-        novo: {
-            p1 : 1,
-            p2 : 2
-        },
-        lista: [
-            'Item1',
-            'Item2',
-            'Item3',
-            'Item4',
-            'Item5',
-        ],
-        helpers: {
-            teste() {
-                return 'TESTE conteudo!'
-            }
-        },
-        produtos: sprodutos
+        
     })
 })
 
